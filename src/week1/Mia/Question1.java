87a4cd57ce3c2f6ed6b32cdecb3ab90b3562eaaf
miasaracuevas <miasaracuevas@yahoo.com>
2024-09-19 14:37:52 -0400

    Q1 - mia

---
+++ src/week1/Mia/Question1.java
@@ -0,0 +1,25 @@
+package week1.Mia;
+
+public class Question1 {
+
+    public static void main(String[] args) {
+
+        System.out.println(oddEven(10));
+
+    }
+
+
+    public static String oddEven(int num) {
+        if (num % 2 == 0) {
+            return "Even";
+
+        }
+        return "Odd";
+    }
+
+}
+
+
+
+
+
