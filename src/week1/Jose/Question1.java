e1a7eaa8308a9bf957a85aecb484f0494db905b4
Chavez-Jose <106994998+Chavez-Jose@users.noreply.github.com>
2024-09-19 11:32:46 -0400

    Solution to question 1

--- src/week1/Jose/Question1.java
+++ src/week1/Jose/Question1.java
@@ -1,4 +1,21 @@
 package week1.Jose;
 
 public class Question1 {
+
+    public static void main(String[] args) {
+
+        System.out.println(oddOrEven(50));
+
+    }
+
+    public static String oddOrEven(int number){
+
+        if (number % 2 == 0){
+            return "Even number: " + number;
+        }else{
+            return "Odd number: " + number;
+        }
+    }
+
+
 }
